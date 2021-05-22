b984c117c430400f506f2d74747f8a83b179b12d
Akash Upadhyay <62694340+Spectre-ak@users.noreply.github.com>
2021-05-22 16:59:05 +0530

    Add files via upload

---
+++ flask/app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def helloworld():
+    return "Hi this image is from github actions and now we can do CI and CD :) "
+
+app.run(debug=True, host='0.0.0.0')
