82958c72ced02520e58db3c6b274a83d9ec17346
Akash Upadhyay <62694340+Spectre-ak@users.noreply.github.com>
2021-05-22 17:08:02 +0530

    Delete helloWorldFlask directory

--- helloWorldFlask/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def helloworld():
-    return "Hi this image is from github sefsefactions and now we can do CI and CD :) "
-
-app.run(debug=True, host='0.0.0.0')
